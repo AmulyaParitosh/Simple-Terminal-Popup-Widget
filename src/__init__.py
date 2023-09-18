21ccb2743fddc897cb9962b77045ba233601a112
Amulya Paritosh <amulya1171@gmail.com>
2023-09-18 12:11:22 +0530

    added license

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
+# Copyright (c) 2023 Amulya Paritosh
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 from .widget import OneLineTerminal
--- terminal.py
+++ terminal.py
@@ -1,3 +1,10 @@
+#!/home/encryptedbee/programfiles/anaconda3/bin/python
+
+# Copyright (c) 2023 Amulya Paritosh
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 import sys
 
 from PyQt6.QtWidgets import QApplication
